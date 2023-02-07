370681b79132ecff508b4d92493859f4bb3a74c5
Raj <i.gooner168@gmail.com>
2023-02-07 20:49:57 +0530

    updated app.js

--- reactv18/src/App.js
+++ reactv18/src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <h2>Welcome to React V18 Concept !!</h2>
       <p> Let's start with Auto Batching !!</p>
+      <hr></hr>
+      <h2>Testing CI/CD using GIT</h2>
     </div>
   );
 }
